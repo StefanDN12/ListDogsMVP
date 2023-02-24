e1dbb0cf958bf2cf80f3d3d49b138862c79aecf8
Stefan <estefan_daniel100@hotmail.com>
2023-02-24 22:33:28 +0100

    Add data clases and model

---
+++ app/src/main/java/com/stefan/listdogsmvp/model/dtClases/Jpg.kt
@@ -0,0 +1,5 @@
+package com.stefan.listdogsmvp.model.dtClases
+
+data class Jpg(
+    val image_url: String
+)
---
+++ app/src/main/java/com/stefan/listdogsmvp/model/dtClases/characterDtModel.kt
@@ -0,0 +1,5 @@
+package com.stefan.listdogsmvp.model.dtClases
+
+data class characterDtModel(
+    val `data`: Data
+)
