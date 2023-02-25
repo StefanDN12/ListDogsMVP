23b3e838d8694cf323f87a0bd73c3147eac7866c
Stefan <estefan_daniel100@hotmail.com>
2023-02-25 09:57:17 +0100

    Implements Retrofit

--- app/src/main/java/com/stefan/listdogsmvp/network/JikanApiImpl.kt
+++ app/src/main/java/com/stefan/listdogsmvp/network/JikanApiImpl.kt
@@ -1,5 +1,13 @@
 package com.stefan.listdogsmvp.network
 
-class JikanApiImpl {
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
+class JikanApiImpl {
+    private fun getRetrofit():Retrofit{
+        return Retrofit.Builder()
+            .baseUrl("https://api.jikan.moe/v4/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
 }
