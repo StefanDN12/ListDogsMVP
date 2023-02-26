97aab7d5c0ebc94583545e31cec60a0039f21362
Stefan <estefan_daniel100@hotmail.com>
2023-02-26 18:34:36 +0100

    Changes presenterConstructor and adds presenter call in jikanModel

--- app/src/main/java/com/stefan/listdogsmvp/interactor/JikanPresenterImpl.kt
+++ app/src/main/java/com/stefan/listdogsmvp/interactor/JikanPresenterImpl.kt
@@ -4,5 +4,5 @@ import com.stefan.listdogsmvp.interfaces.JikanPresenter
 import com.stefan.listdogsmvp.model.JikanModel
 import com.stefan.listdogsmvp.view.ViewJinkan
 
-class JikanPresenterImpl(jikanView: ViewJinkan, jikanModel: JikanModel): JikanPresenter {
+class JikanPresenterImpl(jikanView: ViewJinkan): JikanPresenter {
 }
