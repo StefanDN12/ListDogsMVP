e50bcdb4756cccb948a67f2902778012c7df2a2e
Stefan <estefan_daniel100@hotmail.com>
2023-03-11 16:37:38 +0100

    Add prersenter

--- app/src/main/java/com/stefan/listdogsmvp/view/ViewJinkan.kt
+++ app/src/main/java/com/stefan/listdogsmvp/view/ViewJinkan.kt
@@ -3,10 +3,27 @@ package com.stefan.listdogsmvp.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.stefan.listdogsmvp.R
+import com.stefan.listdogsmvp.interactor.JikanPresenterImpl
+import com.stefan.listdogsmvp.interfaces.JikanPresenter
 
 class ViewJinkan : AppCompatActivity() {
+
+    private lateinit var jikanPresenter:JikanPresenter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        init()
+    }
+
+
+    fun init(){
+        jikanPresenter = JikanPresenterImpl(this)
+    }
+
+
+    fun obtainCharacter(){
+        //jikanPresenter.obtainCharacter()
     }
 }
